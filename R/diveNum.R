103045fafe621c8c0706735a5b886ed009934453
Fer Arce <farcego@gmail.com>
2026-06-26 22:59:56 +0200

    alternative function propossed

--- R/diveNum.R
+++ R/diveNum.R
@@ -21,3 +21,30 @@ DiveNum <- function(Data, column='Depth', dive='dive'){
     Data[,dive] <- num
     return(Data)
 }
+
+
+##' Number dives from a depth time series
+##'
+##' Assigns a sequential dive identifier to each record of a
+##' time-depth profile. A new dive starts each time the profile leaves
+##' the surface and ends when it returns to the surface. Surface
+##' intervals receive the same dive number as the preceding dive.
+##'
+##' @title DiveNum
+##' @param Data A \code{data.frame} containing a depth time series.
+##' @param column Name of the column containing depth values.
+##' @param dive Name of the new column to store the dive index.
+##'
+##' @return The input \code{data.frame} with an additional column
+##'     containing the sequential dive number.
+DiveNum2 <- function(Data, column = "Depth", dive = "dive") {
+
+    if (!column %in% names(Data)) {
+        stop("Column '", column, "' not found.")
+    }
+
+    Data[[dive]] <-
+        ceiling(cumsum(abs(c(0, diff(Data[[column]] == 0)))) / 2)
+
+    Data
+}
